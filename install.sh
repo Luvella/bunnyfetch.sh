22cb986a8f238870567bc3199d7c1325dc55cfb3
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-05-12 22:45:02 -0400

    remove .sh from install file so its the name alone

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 PREFIX=${PREFIX:-/usr}
 
-# TODO: add "$DESTDIR"; "install -Dm 755 bunnyfetch.sh $DESTDIR$PREFIX/bin/bunnyfetch.sh"
-install -Dm 755 bunnyfetch.sh $PREFIX/bin/bunnyfetch.sh
+# TODO: add "$DESTDIR"; "install -Dm 755 bunnyfetch.sh $DESTDIR$PREFIX/bin/bunnyfetch"
+install -Dm 755 bunnyfetch.sh $PREFIX/bin/bunnyfetch
