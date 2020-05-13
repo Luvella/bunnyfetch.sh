d890f492ba4d172fc42e1b4172879d285bdacd25
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-05-13 11:58:57 -0400

    update install script

--- install.sh
+++ install.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-PREFIX=${PREFIX:-/usr}
-
-# TODO: add "$DESTDIR"; "install -Dm 755 bunnyfetch.sh $DESTDIR$PREFIX/bin/bunnyfetch"
-install -Dm 755 bunnyfetch.sh $PREFIX/bin/bunnyfetch
+install -Dm755 bunnyfetch.sh "$DESTDIR"/"${PREFIX:-/usr/local}"/bin/bunnyfetch
